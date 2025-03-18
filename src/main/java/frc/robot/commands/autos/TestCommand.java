42dd490e6ad048c2854c5e8a152437db0e30d4d6
Nick Dupre <132411530+FRC6366RamRodz@users.noreply.github.com>
2025-03-18 15:10:05 -0400

    Auto command working

--- src/main/java/frc/robot/commands/autos/TestCommand.java
+++ src/main/java/frc/robot/commands/autos/TestCommand.java
@@ -5,7 +5,6 @@
 package frc.robot.commands.autos;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.pivot.Pivot;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
