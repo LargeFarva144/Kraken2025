fc1758ff69d787bc9623c551dc69a2af4c67adcc
FRC6366RamRodz <132411530+FRC6366RamRodz@users.noreply.github.com>
2025-09-09 17:40:53 -0400

    Trying Sim

---
+++ src/main/java/frc/robot/subsystems/climb/ClimbSim.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.climb;
+
+import com.ctre.phoenix6.controls.VoltageOut;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+public class ClimbSim implements ClimbIO {
+
+    private SingleJointedArmSim climb;
+    private ArmFeedforward feedforward; 
+    private final VoltageOut voltageOut = new VoltageOut(0).withEnableFOC(true).withUpdateFreqHz(0);
+    boolean isClosedLoop = false;
+
+ public ClimbSim() {
+    climb = 
+    new SingleJointedArmSim(
+        DCMotor.getKrakenX60(1),
+        225,
+        14,
+        Units.inchesToMeters(14),
+        Units.degreesToRadians(150),
+        Units.degreesToRadians(-10),
+        true,
+        0, 0);
+        feedforward = new ArmFeedforward(0, 0, 0, 0);
+    
+ }
+
+
+  @Override
+  public void runVolts(double volts) {
+    _climbMotorK.setControl(voltageOut.withOutput(volts));
+  }
+
+  @Override
+  public double setAngle() {
+    return positionRotations.getValueAsDouble();
+  }
+}
